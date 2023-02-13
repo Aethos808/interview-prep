7ba45dacc98b08bc96c0c5e754b9a5d866a55063
Alex Nigro <alexandercnigro@gmail.com>
2023-02-13 17:14:55 -0500

    fixed terraform file structure

--- src/systemDesign/terraform-practice/aws/main.tf
+++ src/systemDesign/terraform-practice/aws/main.tf
@@ -9,34 +9,28 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
   profile = "terraform"
 }
+
+
+
+
+
+
+
+
 
 # Create a VPC
 #resource "aws_vpc" "example" {
 #  cidr_block = "10.0.0.0/16"
 #}
 
-# get data from existing AWS Cluster
-data "aws_ecs_cluster" "ecs-test" {
-  cluster_name = "test-cluster"
-}
 
-# data "aws_autoscaling_group" "foo" {
-#   name = "foo"
-# }
 
-# resource "aws_ecs_capacity_provider" "ecs-test-capacity-provider" {
-#   name = "ecs-test-capacity-provider"
 
-#   auto_scaling_group_provider {
-#     auto_scaling_group_arn = data.aws_autoscaling_group.foo.arn
-#   }
-# }
 
-#create clone cluster
-resource "aws_ecs_cluster" "ecs-test-clone" {
-  name = "test-cluster-clone"
-}
+
+
+
 
