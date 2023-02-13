7a3a12d45f63f0692ade5332690c15e3b95598c9
Alex Nigro <alexandercnigro@gmail.com>
2023-02-13 17:08:34 -0500

    separated ecs config from other config

--- src/systemDesign/terraform-practice/aws/ecs.tf
+++ src/systemDesign/terraform-practice/aws/ecs.tf
@@ -1,23 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region  = "us-east-1"
-  profile = "terraform"
-}
-
-# Create a VPC
-#resource "aws_vpc" "example" {
-#  cidr_block = "10.0.0.0/16"
-#}
-
 # get data from existing AWS Cluster
 data "aws_ecs_cluster" "ecs-test" {
   cluster_name = "test-cluster"
@@ -38,5 +18,8 @@ data "aws_ecs_cluster" "ecs-test" {
 #create clone cluster
 resource "aws_ecs_cluster" "ecs-test-clone" {
   name = "test-cluster-clone"
+  # setting {
+  # name = "containerInsights"
+  # value = data.aws_ecs_cluster.ecs-test.setting.0.value
+  # }
 }
-
