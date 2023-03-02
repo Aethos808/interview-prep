8241c7b463e40bbb3837bde608efe6f363fcfe5a
Alex Nigro <alexandercnigro@gmail.com>
2023-03-02 15:13:37 -0500

    separated ecs stuff from aws provider in tf main

--- src/systemDesign/terraform-practice/aws/ecs.tf
+++ src/systemDesign/terraform-practice/aws/ecs.tf
@@ -3,6 +3,7 @@ data "aws_ecs_cluster" "ecs-test" {
   cluster_name = "test-cluster"
 }
 
+
 # data "aws_autoscaling_group" "foo" {
 #   name = "foo"
 # }
@@ -18,8 +19,19 @@ data "aws_ecs_cluster" "ecs-test" {
 #create clone cluster
 resource "aws_ecs_cluster" "ecs-test-clone" {
   name = "test-cluster-clone"
-  # setting {
-  # name = "containerInsights"
-  # value = data.aws_ecs_cluster.ecs-test.setting.0.value
-  # }
+  #   setting {
+
+  #     data.aws_ecs_cluster.ecs-test.setting
+  #   }
+}
+resource "aws_ecs_cluster_capacity_providers" "ecs-test-clone" {
+  cluster_name = "test-cluster-clone"
+
+  capacity_providers = ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE"
+  }
 }
